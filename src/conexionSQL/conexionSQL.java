579560f748afc285efab31dff786fb9999d80558
leecanon <victor.chuquin@upeu.edu.pe>
2022-01-15 17:39:19 -0500

    agregarsql-funcionando

---
+++ src/conexionSQL/conexionSQL.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionSQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Usuario
+ */
+public class conexionSQL {
+    
+    Connection conectar=null;
+    
+    public Connection conexion(){
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar=DriverManager.getConnection("jdbc:mysql://localhost/escuela","root","");
+            JOptionPane.showMessageDialog(null, "Conexion Exitosa");
+            
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error de conexion " + e.getMessage());
+        }
+        return conectar;
+    }
+}
